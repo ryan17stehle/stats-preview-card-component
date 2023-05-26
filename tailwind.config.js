574b391701102a72a7c3aba4d31dcd314852c7a2
ryan17stehle <34487497+ryan17stehle@users.noreply.github.com>
2023-05-25 20:31:57 -0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'purple': '#ab5cdb',
+        'violet': '#1b1937',
+        'white': '#ffffff',
+        'black': '#0a0c1c',
+        'gray': '#979797',
+      },
+      fontSize: {
+        base: ['15px', '25px'],
+        lg: ['28px', '32px']
       },
     },
   },
