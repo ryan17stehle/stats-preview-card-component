d8bb731607381f7ed24039639c575501c88289f2
ryan17stehle <34487497+ryan17stehle@users.noreply.github.com>
2023-05-25 20:43:38 -0700

    fixed background image color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,24 @@ import desktopImg from '../../public/image-header-desktop.jpg'
 export default function Home() {
   return (
     <main 
-      className={`min-w-[280px] max-w-[425px] bg-violet rounded-lg xl:flex xl:max-w-[1110px]`}
+      className={`min-w-[280px] max-w-[425px] bg-purple rounded-lg xl:flex xl:max-w-[1110px]`}
     >
       <section className="xl:order-2">
         <Image
           src={desktopImg}
           alt="people working in an office"
-          className="hidden mb-8 max-w-full rounded-t-lg mix-blend-multiply opacity-75 bg-purple
+          className="hidden max-w-full rounded-t-lg mix-blend-multiply opacity-75 bg-purple
           xl:block xl:h-full"
         />
         <Image
           src={mobileImg}
           alt="people working in an office"
-          className="mb-8 block max-w-full rounded-t-lg mix-blend-multiply opacity-75 bg-purple
+          className="block max-w-full rounded-t-lg mix-blend-multiply opacity-75 bg-purple
           xl:hidden"
         />
       </section>
-      <section className="grid place-items-center xl:order-1 xl:text-left">
-        <h1 className="text-white text-lg font-bold text-center mb-4 w-4/6">
+      <section className=" bg-violet grid place-items-center xl:order-1 xl:text-left">
+        <h1 className="text-white text-lg font-bold text-center mb-4 mt-8 w-4/6">
           Get <span className="text-purple">insights</span> that help your business grow.
         </h1>
         <p className="text-base text-gray text-center mb-8 w-4/6">
